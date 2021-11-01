560b5f3e0c4750646419ff3aa46c383774f85301
RinZ26 <ivs1996@mail.ru>
2021-11-01 19:29:37 +0500

    1. HbmTracker. [#287705]

---
+++ src/main/java/ru/job4j/tracker/HbmTracker.java
@@ -0,0 +1,109 @@
+package ru.job4j.tracker;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import ru.job4j.tracker.entity.Item;
+
+import java.util.List;
+
+public class HbmTracker implements Store, AutoCloseable {
+    private final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+            .configure().build();
+    private final SessionFactory sf = new MetadataSources(registry)
+            .buildMetadata().buildSessionFactory();
+
+    @Override
+    public void init() {
+    }
+
+    @Override
+    public Item add(Item item) {
+        try (Session session = sf.openSession()) {
+            session.beginTransaction();
+            session.save(item);
+            session.getTransaction().commit();
+        }
+        return item;
+    }
+
+    @Override
+    public boolean replace(Integer id, Item item) {
+        boolean result = true;
+        try (Session session = sf.openSession()) {
+            session.beginTransaction();
+            session.update(item);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            result = false;
+        }
+        return result;
+    }
+
+    /**
+     * Интересный "хак" с удалением по id, когда мы вместо доп. запроса findById просто
+     * делаем фейк заявку и удаляем её
+     *
+     * @param id
+     * @return
+     */
+    @Override
+    public boolean delete(Integer id) {
+        boolean result = true;
+        try (Session session = sf.openSession()) {
+            session.beginTransaction();
+
+            Item item = new Item(null);
+            item.setId(id);
+            session.delete(item);
+
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            result = false;
+        }
+        return result;
+    }
+
+    @Override
+    public List<Item> findAll() {
+        List<Item> result;
+        try (Session session = sf.openSession()) {
+            session.beginTransaction();
+            result = session.createQuery("from ru.job4j.tracker.entity.Item").list();
+            session.getTransaction().commit();
+        }
+        return result;
+    }
+
+    @Override
+    public List<Item> findByName(String key) {
+        List<Item> result;
+        try (Session session = sf.openSession()) {
+            session.beginTransaction();
+            result = session.createQuery("from ru.job4j.tracker.entity.Item " +
+                    "where name = :name ").setParameter("name", key).list();
+            session.getTransaction().commit();
+            session.close();
+        }
+        return result;
+    }
+
+    @Override
+    public Item findById(Integer id) {
+        Item result;
+        try (Session session = sf.openSession()) {
+            session.beginTransaction();
+            result = session.get(Item.class, id);
+            session.getTransaction().commit();
+            session.close();
+        }
+        return result;
+    }
+
+    @Override
+    public void close() throws Exception {
+        StandardServiceRegistryBuilder.destroy(registry);
+    }
+}
